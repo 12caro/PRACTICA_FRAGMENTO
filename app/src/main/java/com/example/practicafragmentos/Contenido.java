0d44d25845a0dbd9e53de51332b51cafdf321870
DESKTOP-65NT5MH\Usuario <mariana123>
2019-03-24 23:03:38 -0500

    no message

---
+++ app/src/main/java/com/example/practicafragmentos/Contenido.java
@@ -0,0 +1,160 @@
+package com.example.practicafragmentos;
+
+public class Contenido {
+
+
+        static String[] Titulos = {
+
+                "TOUR TACNA",
+                "TOUR CUSCO",
+                "TOUR AREQUIPA",
+                "TOUR LIMA",
+                "TOUR NAZCA",
+                "TOUR PARACAS",
+                "TOUR HUARAZ",
+                "TOUR PUERTO_MALDONAO",
+                "TOUR PUNO",
+                "TOUR CHAN_CHAN",
+                "TOUR OLLANTAYTAMBO",
+                "TOUR PISAC "};
+
+
+        static String[] Parrafos =
+                {"Tacna es una ciudad del sur de Perú, cerca de la frontera con Chile. El Paseo Cívico de Tacna se encuentra en su centro y alberga la Catedral de Tacna, de estilo neorrenacentista. Cerca, está el monumento Arco Parabólico, dedicado a los soldados de la Guerra del Pacífico, y el Museo \n" +
+                        "En el centro de la ciudad, está el Parque de la Locomotora donde hay un tren que usaron las tropas peruanas durante la guerra con Chile. El Museo Ferroviario Nacional se ubica en la antigua estación de trenes y tiene exhibiciones sobre la historia ferroviaria. La Casa Zela fue ",
+                        "Cuzco es una ciudad de los Andes peruanos que fue la capital del Imperio Inca y es conocida por sus restos arqueológicos y la arquitectura colonial española. La Plaza de Armas es el centro de la ciudad antigua, con galerías, balcones de madera tallada y ruinas de murallas incas.\n" +
+                                "Los sitios icónicos españoles, como la catedral del siglo XVII, usaron la albañilería inca como base y para su construcción. Sobre la ciudad, el complejo de Sacsayhuamán sobre un cerro tiene murallas de piedra seca características en zigzag, junto con vistas sobre los ",
+
+                        "Arequipa es la capital de la época colonial de la región de Arequipa en Perú. La rodean 3 volcanes y cuenta con edificios barrocos construidos de sillar, una piedra volcánica blanca. En su centro histórico, se encuentra la Plaza de Armas, una imponente \n" +
+                                "El Monasterio de Santa Catalina, un convento fundado en 1579, está amurallado en un complejo similar a una villa que cuenta con claustros y calles de adoquines. La Casa del Moral, una mansión del siglo XVIII convertida en museo, alberga mapas coloniales, antiguas monedas y arte re",
+
+                        "Lima es la capital de Perú ubicada en la árida costa del Pacífico del país. Pese a que su centro colonial se conserva, es una desbordante metrópolis y una de las ciudades más grandes de Sudamérica. \n" +
+                                "Otros sitios icónicos del centro .",
+
+                        "Nazca es una ciudad peruana ubicada en la región centro-sur del Perú, capital de la homónima provincia de Nazca, situada en la margen derecha del río Aja, afluente del Río Grande. Situada a 439 km al sur de la ciudad de Lima, en un estrecho valle a 520 msnm. Nazca es una ciudad muy activa,",
+
+                        "Paracas es una ciudad de la costa oeste de Perú. Es conocida por sus playas, como El Chaco, ubicada en la bahía resguardada de Paracas. La ciudad es un punto de partida hacia las islas Ballestas deshabitadas, hogar de lobos marinos, pelícanos y pingüinos de Humboldt. ",
+
+                        "La ciudad de Huaraz está en el Callejón de Huaylas, un valle del norte de Perú. Es la capital del departamento de Ancash y está situada a más de 3000 metros sobre el nivel del mar. Los picos nevados de la Cordillera Blanca conforman su llamativo horizonte oriental.  \n" +
+                                "Huaraz es un lugar muy popular para hacer senderismo y alpinismo en las montañas de la Cordillera Blanca y en la zona de la Cordillera Negra, al oeste de la localidad. El Museo Arqueológico de Ancash cuenta con una gran variedad de artefactos precolombinos,.\n",
+
+                        "Puerto Maldonado es la capital de la región de Madre de Dios, en el sudeste de Perú. También es conocida como la puerta de entrada a la selva amazónica meridional. La céntrica torre del Obelisco ofrece vistas de la ciudad, además de exposiciones sobre la historia local.\n" +
+                                "En el interior de la reserva, la gran Collpa de Guacamayos de \n",
+
+                        "Puno es una ciudad del sur de Perú ubicada junto al lago Titicaca, uno de los lagos más grandes de Sudamérica y el cuerpo de agua navegable más alto del mundo. La ciudad es un núcleo comercial regionaL\n",
+
+
+                        "Chan Chan es una ciudad precolombina de adobe, construida en la costa norte del Perú por los chimúes. Es la ciudad construida en adobe más grande de América\u200B y la segunda en el mundo. Se ubica al noroeste del área metropolitana de Trujillo \n",
+
+
+                        "Ollantaytambo es un pueblo del Valle Sagrado del sur de Perú que está situado junto al río Urubamba, entre montañas cubiertas de nieve. Es conocido por las ruinas de Ollantaytambo, una enorme fortaleza inca con grandes terrazas de piedra en una colina.  \n",
+
+
+                        "Písac es un complejo arqueológico que está en el distrito homónimo de la provincia de Calca,está ubicado a 30 kilómetros de la ciudad del Cusco, \u200B en el Perú. El yacimiento arqueológico es\n",
+
+                };
+
+        static String[] Detalles = {"\n" + "DETALLES:"+ "\n" + "\n" +
+                                            "PRECIO : S/.600" + "\n" +
+                                            "DURACION  : 4 dias / 2 noches " +"\n" +
+                                            "ACTIVIDADES : Traslado en Bus: Lima - Tacna o Tacna - Lima." ,
+
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.500" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - Cusco o Tacna - cusco." ,
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.400" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - arequipa o arequipa - Lima." ,
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.600" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - Tacna o Tacna - Lima." ,
+
+
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.500" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - nazca o nazca - Lima." ,
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.450" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - paracas o paracas - Lima." ,
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.600" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - huaraz o huaraz - Lima." ,
+
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.600" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - puerto maldonado o puerto maldonado - Lima." ,
+
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.600" + "\n" +
+                        "DURACION  : 4 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - puno o puno - Lima." ,
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.600" + "\n" +
+                        "DURACION  : 3 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - chan chan o chan chan - Lima." ,
+
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.600" + "\n" +
+                        "DURACION  : 2 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - OLLANTAYTAMBO o OLLANTAYTAMBO - Lima." ,
+
+
+                "\n" + "DETALLES:"+ "\n" + "\n" +
+                        "PRECIO : S/.600" + "\n" +
+                        "DURACION  : 2 dias / 2 noches " +"\n" +
+                        "ACTIVIDADES : Traslado en Bus: Lima - PISAC o PISAC - Lima." };
+
+
+        static int[] sImages = {R.drawable.tacna,
+                R.drawable.cusco,
+                R.drawable.arequipa,
+                R.drawable.lima,
+                R.drawable.nazca,
+                R.drawable.paracas,
+                R.drawable.huaraz,
+                R.drawable.puerto_maldonado,
+                R.drawable.puno,
+                R.drawable.chan_chan,
+                R.drawable.ollantaytambo,
+                R.drawable.pisac
+
+        };
+
+
+
+
+
+
+
+
+
+
+
+}
